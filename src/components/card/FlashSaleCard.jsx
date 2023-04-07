4afc9df261ccc56e432aedb2246b97d3d7e43b6f
Tushar@8640 <imtantushar44@gmail.com>
2023-04-08 01:22:24 +0600

    flas sell complete

--- src/components/card/FlashSaleCard.jsx
+++ src/components/card/FlashSaleCard.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 
 const FlashSaleCard = ({ product }) => {
-    const [over,setOver] = useState(false)
+  const [over, setOver] = useState(false);
   const {
     title,
     category,
@@ -11,27 +11,32 @@ const FlashSaleCard = ({ product }) => {
     discountPrice,
     new: sale,
   } = product || {};
-  console.log(over)
+  console.log(over);
   return (
     <>
-      <div className="max-w-[280] h-[508px] ring-[1px] ring-[#F2F2F2] bg-white text-gray-900 p-[20px] "
-       onMouseOver={()=>setOver(true)} 
-       onMouseOut={()=>setOver(false)}
+      <div
+        className={`max-w-[280] h-[508px] ${
+          !over && "ring-[1px]"
+        } ring-[#F2F2F2] bg-white text-gray-900  `}
+        onMouseOver={() => setOver(true)}
+        onMouseOut={() => setOver(false)}
       >
-        <img
-          src={image}
-          alt=""
-          className="object-contain object-center w-full rounded-md h-[131px] bg-white my-3 transform group-hover:-scale-x-100"
-        />
-        <div className="mt-12 mb-1 flex flex-col justify-between p-2">
-          <span className="block text-[12px]  text-[#8B928F] mb-3">
-            {category}
-          </span>
-          <h2 className="text-[16px] text-[#17201D] font-normal  tracking-wide">
-            {title}
-          </h2>
+        <div className={`p-[15px] ${over && "ring-[1px]"} ring-[#F2F2F2]`}>
+          <img
+            src={image}
+            alt=""
+            className="object-contain object-center w-full rounded-md h-[131px] bg-white my-3 transform group-hover:-scale-x-100"
+          />
+          <div className="mt-10 mb-1 flex flex-col justify-between ">
+            <span className="block text-[12px]  text-[#8B928F] mb-3">
+              {category}
+            </span>
+            <h2 className="text-[16px] text-[#17201D] font-normal  tracking-wide">
+              {title}
+            </h2>
+          </div>
         </div>
-        <div className={`${!over ? "visible":"hidden"} my-2 p-2`}>
+        <div className={`${!over ? "visible" : "hidden"} my-1 p-2 h-[80px`}>
           <div className="flex gap-x-2">
             <div className="flex items-center">
               <span>
@@ -128,64 +133,76 @@ const FlashSaleCard = ({ product }) => {
         </div>
 
         {/* button show on hover  */}
-        <div className={`${over ? "visible":"hidden"} `} >
-          <button>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="w-6 h-6"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z"
-              />
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
-              />
-            </svg>
-          </button>
-          <button>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="w-6 h-6"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
-              />
-            </svg>
-          </button>
-          <button>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="w-6 h-6"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M3 7.5L7.5 3m0 0L12 7.5M7.5 3v13.5m13.5 0L16.5 21m0 0L12 16.5m4.5 4.5V7.5"
-              />
-            </svg>
+        <div
+          className={`${
+            over ? "visible" : "hidden"
+          } flex justify-around my-1 p-2 h-[80px] items-center`}
+        >
+          <div className="border-r-2  pr-3 flex justify-center items-center">
+            <button className=" hover:bg-black rounded-full h-10 w-10 flex justify-center items-center group transition-all duration-100">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-5 h-5 group-hover:text-white transition-all duration-100"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z"
+                />
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+                />
+              </svg>
+            </button>
+          </div>
+          <div className=" flex justify-center items-center ">
+            <button className="  hover:bg-black rounded-full h-10 w-10 flex justify-center items-center group transition-all duration-100">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-5 h-5 group-hover:text-white transition-all duration-100"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
+                />
+              </svg>
+            </button>
+          </div>
+          <div className=" border-l-2 pl-2">
+            <button className=" hover:bg-black rounded-full h-10 w-10 flex justify-center items-center group transition-all duration-100">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-5 h-5 group-hover:text-white transition-all duration-100"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M3 7.5L7.5 3m0 0L12 7.5M7.5 3v13.5m13.5 0L16.5 21m0 0L12 16.5m4.5 4.5V7.5"
+                />
+              </svg>
+            </button>
+          </div>
+        </div>
+        <div className={`p-[10px] ${over && "ring-[1px] h-[100px]"} ring-[#F2F2F2]`}>
+          <button className="w-full bg-[#96AEA1]/10 mt-2 py-3 text-[#5B9982] font-medium text-[15px]">
+            Add To Cart
           </button>
         </div>
-        <button className="w-full bg-[#96AEA1]/10 mt-3 py-3 text-[#5B9982] font-medium text-[15px]">
-          Add To Cart
-        </button>
       </div>
     </>
   );
